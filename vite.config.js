a543563edf4a643b280c883352a8586a07ebdb66
Hisham-ahmed14 <hishamahmed.ea@gmail.com>
2026-04-14 14:41:28 +0530

    Fixed PWA manifest and dev loading bugs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
       manifest: {
         name: 'Discipline Tracker',
         short_name: 'Tracker',
